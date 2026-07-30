9fa401cfb8f3a202f70fce2e4457bf2ddade4682
Miguelshre <ooomonteiro42@gmail.com>
2026-07-30 17:30:21 +0100

    melhorar o controller criação do model user

--- controllers/controller.php
+++ controllers/controller.php
@@ -1,14 +1,16 @@
 <?php
 include_once 'config/database.php';
-
+include_once 'models/user.php';
 
 class Controller
 {
     private $db;
+    private $user;
 
     public function __construct()
     {
         $database = new Database();
         $this->db = $database->getConnection();
+        $this->user = new User($this->db);
     }
 }
