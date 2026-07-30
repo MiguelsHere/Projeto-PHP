1843555cfc8c2842341338a9347d0360a4fac7e9
Miguelshre <ooomonteiro42@gmail.com>
2026-07-30 19:09:55 +0100

    Melhorar css e view homepage

--- views/homepage.php
+++ views/homepage.php
@@ -8,12 +8,15 @@
 </head>
 
 <body>
-    <header>Workout Maker</header>
+    <header>
+        <h1>Workout Maker</h1>
+    </header>
     <nav><a href="views/profile.php">Perfil</a></nav>
     <main>
-        <h1>Bem-Vindo ao<br>Workout Maker</h1>
-        <h2>O que é o Workout maker?</h2>
-        <p></p>
+        <h2>Bem-Vindo ao<br>Workout Maker</h2>
+        <h3>O que é o Workout maker?</h3>
+        <p>O Workout maker é uma aplicação web que dá ao utilizador todos os essencias para criar, encontrar, publicar e avaliar workouts!<br>
+            Encontre workouts efetivos de acordo com o seu nivel e equipamento disponivel tendo uma vasta base de dados de exercicsios para criar o seu proprio workout ou usar um workout feito pela comunidade!</p>
     </main>
     <footer>
         <p>criado por Miguel Monteiro</p>
