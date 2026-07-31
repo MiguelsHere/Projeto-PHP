3c75ac043422634392a942d990dd18bf354fbd89
Miguelshre <ooomonteiro42@gmail.com>
2026-07-31 20:33:08 +0100

    Simplificar coneceção da base de dados

--- config/database.php
+++ config/database.php
@@ -1,19 +1,17 @@
 <?php
 
-class Database {
-    private $host = "localhost";
-    private $db_name = "workout_db";
-    private $username = "root"; 
-    private $password = "";
+class Database
+{
     public $conn;
 
-    public function getConnection() {
+    public function getConnection()
+    {
         $this->conn = null;
         try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn = new PDO("mysql:host=localhost;dbname=workout_db", "root", "");
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->conn->exec("set names utf8");
-        } catch(PDOException $exception) {
+        } catch (PDOException $exception) {
             echo "Connection Error: " . $exception->getMessage();
         }
         return $this->conn;
