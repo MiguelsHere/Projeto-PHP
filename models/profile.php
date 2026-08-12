92058701d2e72fa5990fa1591eaeb7c1d4a1c302
MiguelsHere <ooomonteiro42@gmail.com>
2026-08-12 14:54:05 +0100

    Melhorar views, model e base de dados Adicionar tag meta discription á header das views Melhorar metodo register e metodo login do model profile

--- models/profile.php
+++ models/profile.php
@@ -25,12 +25,26 @@ class Profile
     {
         $query = "SELECT user_name, created_in FROM profile WHERE is_public = 1 ORDER BY created_in DESC";
         $stmt = $this->conn->prepare($query);
+
         $stmt->execute();
+
         return $stmt;
     }
 
+    //Depois fazer algo com session para os returns
     public function register()
     {
+        $query = "SELECT user_name, email from profile where user_name = :user_name or email = :email";
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->bindParam(":user_name", $this->user_name);
+        $stmt->bindParam(":email", $this->email);
+        $stmt->execute();
+        
+        if($stmt->rowCount() > 0) {
+            return false;
+        }
+
         $hash = password_hash($this->password, PASSWORD_ARGON2ID);
 
         $query = "INSERT INTO profile(user_name, password, email) VALUES(:user_name, :password, :email)";
@@ -45,7 +59,22 @@ class Profile
 
     public function login()
     {
-        $query = "";
+        $query = "SELECT password FROM profile WHERE user_name = :user_name OR email = :email";
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->bindParam(":user_name", $this->user_name);
+        $stmt->bindParam(":email", $this->email);
+
+        if ($stmt->execute()) {
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if ($row && password_verify($this->password, $row['password'])) {
+
+                return true;
+            }
+        }
+        else return $stmt->execute();
     }
 
     public function update_no_auth()
